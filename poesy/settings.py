2363ac74be242eee599b6e994b51376326ce93c0
alisani081 <960395b7a1e79fe610d98498afeab15d68cbe064>
2022-12-14 12:48:51 +0100

    staticfiles fix II

--- poesy/settings.py
+++ poesy/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -154,6 +155,8 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # All-Auth configuration
 ACCOUNT_EMAIL_REQUIRED = True
